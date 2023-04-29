e9a1cb3d099b1a67fdfb2fb7aeed01b948c7d998
Kelvin Marcondes <kelvindesouza@hotmail.com>
2023-04-29 18:12:27 -0300

    Content negotiation via header parameter

--- src/main/java/com/marcondes/courserestjava/config/WebConfig.java
+++ src/main/java/com/marcondes/courserestjava/config/WebConfig.java
@@ -1,14 +1,31 @@
 package com.marcondes.courserestjava.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+//  VIA QUERY PARAM
+//    @Override
+//    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//
+//        configurer.favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(true)
+//                .useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
+//                .mediaType("json", MediaType.APPLICATION_JSON)
+//                .mediaType("xml", MediaType.APPLICATION_XML);
+//    }
+
+
+//  VIA HEADER PARAM
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        WebMvcConfigurer.super.configureContentNegotiation(configurer);
+
+        configurer.favorParameter(true).ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
     }
 }
